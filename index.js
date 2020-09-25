841ebd8580e5a396022298b2c767ec38a72d6170
Long Huynh <Longhuynh741@yahoo.com>
2020-09-24 20:37:19 -0400

    added questions array in index.js

--- index.js
+++ index.js
@@ -5,9 +5,41 @@ function promptUser() {
     return inquirer.prompt([
 
         {   
-            
+            type: "input",
+            name: "title",
+            message: "What would you like to name your ReadMe?"
         }
 
+        {   
+            type: "input",
+            name: "description",
+            message: "How would you briefly describe your project?"
+        }
+
+
+        {   
+            type: "input",
+            name: "installation",
+            message: "How does a User the application? Write instructions here "
+        }
+
+        {   
+            type: "input",
+            name: "usage",
+            message: "How does a User properly use this application? Write Usage iinstructions here"
+        }
+
+        {   
+            type: "input",
+            name: "contributions",
+            message: "What are the contributions of this project?"
+        }
+
+        {   
+            type: "input",
+            name: "license",
+            message: "What license are you using?"
+        }
 
 
 
